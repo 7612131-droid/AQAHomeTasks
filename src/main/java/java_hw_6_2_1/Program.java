bb27afe84c1a6f36f40db779645a4852579dd10c
7612131-droid <7612131@gmail.com>
2026-07-22 11:30:28 +0300

    homework 6_2 task_1

--- src/main/java/java_hw_6_2_1/Program.java
+++ src/main/java/java_hw_6_2_1/Program.java
@@ -1,34 +1,37 @@
-//package java_hw_6_2_1;
-//
-//public class Program {
-//    abstract class Shape {
-//
-//    // Abstract method
-//    abstract double area();
-//
-//    // Concrete method
-//    void display() {
-//        System.out.println("This is a shape");
-//    }
-//}
-//
-//    class Circle extends Shape {
-//
-//        int radius = 5;
-//
-//        // Implementing abstract method
-//        @Override
-//        double area() {
-//            return 3.14 * radius * radius;
-//        }
-//    }
-//
-//    public class Main {
-//        public static void main(String[] args) {
-//
-//            Shape shape = new Circle();  // Upcasting
-//            shape.display();
-//            System.out.println("Area of Circle: " + shape.area());
-//        }
-//    }
-//}
+package java_hw_6_2_1;
+
+public class Program {
+    public static void main(String[] args) {
+        Figure square = new Square(5);
+        Figure triangle= new Triangle(6,4,4,5,6);
+        Figure circle= new Circle(7);
+
+
+        System.out.println("Square information:");
+        System.out.println(square);
+        System.out.println("Square area:");
+        System.out.println(square.getArea());
+        System.out.println("Square perimeter:");
+        System.out.println(square.getPerimeter());
+
+        System.out.println();
+
+        System.out.println("Triangle information:");
+        System.out.println(triangle);
+        System.out.println("Triangle area:");
+        System.out.println(triangle.getArea());
+        System.out.println("Triangle perimeter:");
+        System.out.println(triangle.getPerimeter());
+
+        System.out.println();
+
+        System.out.println("Circle information:");
+        System.out.println(circle);
+        System.out.println("Circle area:");
+        System.out.println(circle.getArea());
+        System.out.println("Circle perimeter:");
+        System.out.println(circle.getPerimeter());
+
+    }
+
+}
